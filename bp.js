6879bb0bbef970e2d9008d666df62b58e1679918
bungacally <125546844+alyaacallayayaa@users.noreply.github.com>
2024-05-05 23:40:53 +0700

    Add files via upload

---
+++ bp.js
@@ -0,0 +1,12 @@
+
+<script>
+
+function myFunction() {
+  var x = document.createElement("IMG");
+  x.setAttribute("src", "img/bpfoto.jpg");
+  x.setAttribute("width", "304");
+  x.setAttribute("height", "228");
+  x.setAttribute("alt", "Blackpink Image");
+  document.body.appendChild(x);
+}
+</script>
